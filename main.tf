e07d7178b5deec2a11e136070dbad6be5326cfd0
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2024-05-23 21:20:16 +0530

    update code

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ resource "azurerm_mssql_server" "sql_server"{
     resource_group_name          = var.resource_group_output[each.value.resource_group_name].name
     location                     = var.resource_group_output[each.value.resource_group_name].location
     version                      = each.value.version
-    administrator_login          = sql_server.value.azuread_administrator.azuread_authentication_only == true ? null : each.value.administrator_login_name
-    administrator_login_password = sql_server.value.azuread_administrator.azuread_authentication_only == true ? null : each.value.administrator_login_password
+    administrator_login          = each.value.azuread_administrator.azuread_authentication_only == true ? null : each.value.administrator_login_name
+    administrator_login_password = each.value.azuread_administrator.azuread_authentication_only == true ? null : each.value.administrator_login_password
     minimum_tls_version          = each.value.minimum_tls_version
     public_network_access_enabled = each.value.public_network_access_enabled
 
 
     dynamic "azuread_administrator" {
-      for_each = each.value.administrator_login_name != null || length(each.value.administrator_login_name) != "0" ? [] : sql_server.value.azuread_administrator
+      for_each = each.value.administrator_login_name != null || length(each.value.administrator_login_name) != "0" ? [] : each.value.azuread_administrator
       content {
         azuread_authentication_only = azuread_administrator.value.azuread_authentication_only
         login_username  = var.user_assigned_identity_output[azuread_administrator.value.login_username].name
@@ -21,14 +21,14 @@ resource "azurerm_mssql_server" "sql_server"{
     }
 
     dynamic "identity" {
-      for_each = sql_server.value.azuread_administrator
+      for_each = each.value.azuread_administrator
       content {
         type = "UserAssigned"
         identity_ids = var.user_assigned_identity_output[azuread_administrator.value.login_username].id
       }
     } 
 
-    primary_user_assigned_identity_id            = var.user_assigned_identity_output[sql_server.value.azuread_administrator.login_username].id
+    primary_user_assigned_identity_id            = var.user_assigned_identity_output[each.value.azuread_administrator.login_username].id
     transparent_data_encryption_key_vault_key_id = var.key_vault_output[each.value.key_vault_name].id
     
 }
